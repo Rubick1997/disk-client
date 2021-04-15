b66bc8d6c35b09c2ee7c565a23287c547878ec8a
Rustam <rustam905@gmail.com>
2021-04-14 20:43:29 -0700

    Redux for user

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { store } from "./reducers";
+import { Provider } from "react-redux";
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
 );
